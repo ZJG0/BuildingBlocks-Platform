53658d22a180e15faeadc89fb880a7969fe3be3a
ZJG0 <1173939915@qq.com>
2022-11-09 10:25:10 +0800

    test comparators

--- evaluatePerformance.py
+++ evaluatePerformance.py
@@ -2,7 +2,7 @@
 Author: ZJG
 Date: 2022-11-08 22:21:51
 LastEditors: ZJG
-LastEditTime: 2022-11-09 09:27:40
+LastEditTime: 2022-11-09 10:23:53
 '''
 
 import itertools
@@ -31,7 +31,8 @@ def _get_random_test_tensor(*args, **kwargs):
     
 def test_comparators():
     """Test comparators (>, >=, <, <=, ==, !=)"""
-    for comp in ["gt", "ge", "lt", "le", "eq", "ne"]:
+    # ["gt", "ge", "lt", "le", "eq", "ne"]
+    for comp in ["gt"]:
         for tensor_type in [lambda x: x, MPCTensor]:
             tensor1 = _get_random_test_tensor(is_float=True)
             tensor2 = _get_random_test_tensor(is_float=True)
@@ -40,18 +41,21 @@ def test_comparators():
             encrypted_tensor2 = tensor_type(tensor2)
             reference = getattr(tensor1, comp)(tensor2).float()
             encrypted_out = getattr(encrypted_tensor1, comp)(encrypted_tensor2)
+            print(tensor_type)
+            print("     reference"+str(reference))
+            print("     encrypted_out"+str(encrypted_out.get_plain_text()))
 
             # _check(encrypted_out, reference, "%s comparator failed" % comp)
 
             # Check deterministic example to guarantee all combinations
-            tensor1 = torch.tensor([2.0, 3.0, 1.0, 2.0, 2.0])
-            tensor2 = torch.tensor([2.0, 2.0, 2.0, 3.0, 1.0])
+            # tensor1 = torch.tensor([2.0, 3.0, 1.0, 2.0, 2.0])
+            # tensor2 = torch.tensor([2.0, 2.0, 2.0, 3.0, 1.0])
 
-            encrypted_tensor1 = MPCTensor(tensor1)
-            encrypted_tensor2 = tensor_type(tensor2)
+            # encrypted_tensor1 = MPCTensor(tensor1)
+            # encrypted_tensor2 = tensor_type(tensor2)
 
-            reference = getattr(tensor1, comp)(tensor2).float()
-            encrypted_out = getattr(encrypted_tensor1, comp)(encrypted_tensor2)
+            # reference = getattr(tensor1, comp)(tensor2).float()
+            # encrypted_out = getattr(encrypted_tensor1, comp)(encrypted_tensor2)
 
             # self._check(encrypted_out, reference, "%s comparator failed" % comp)
             
